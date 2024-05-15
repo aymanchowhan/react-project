997ab04cdb5071a4e03acd41621a2f02894dc6aa
Ayman Chowhan <aymanchowhan@gmail.com>
2024-05-15 23:20:48 +0530

    multiple fonts

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,22 @@ import React from 'react'
 function Hero() {
     return (
         <div className='grid grid-cols-2 h-[70vh] pt-56 container m-auto'>
-            <div className='h-full bg-red-500'>
-                The best
-                landing page for
-                your digital product.
-
-                Our cloud computing platform was built with simplicity
-                so managing infrastructure is easy.
+            <div style={{fontSize:'55px'}} className='h-full bg-red-500 leading-[5rem] h2-title text-white' >
+           <strong> The best <br />
+            landing page for <br />
+            your digital <br />
+             product.</strong>
+         <div style={{fontSize:'20px'}} className="p-subtitle leading-6"> 
+                   <p>Our cloud computing platform was built with simplicity <br />
+                     so managing infrastructure is easy.</p> 
+         </div>
+         <div style={{fontSize:'25px'}} className={'h-30 flex justify-between text-white px-10 text-lg py-4 p-12 h2-title'} >
+    <button id="myBtn" style={{fontSize:'18px'}} className='relative cursor-pointer hover:bg-blue-600 duration-500 hover:text-white rounded-md p-[20px] my-1'>Discover Now</button>
+       </div>
             </div>
+        
+
+
             <div className='h-full bg-green-500'></div>
 
         </div>
